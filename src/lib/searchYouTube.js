d09c56bdb8c0816e552670bebf247c14d18b1b7b
brandontiqui <btiqui@gmail.com>
2016-08-13 17:09:42 -0700

    implemented youtube api request

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,6 +1,22 @@
-var searchYouTube = (options, callback) => {
-  // TODO
-  return window.exampleVideoData;
+var searchYouTube = ({key, query, max = 5}, callback) => {
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    part: 'snippet',
+    key: key,
+    q: query,
+    maxResults: max,
+    type: 'video',
+    videoEmbeddable: 'true'
+  })
+  .done(({items}) => {
+    if (callback) {
+      callback(items);
+    }
+  })
+  .fail(({responseJSON}) => {
+    responseJSON.error.errors.forEach((err) =>
+      console.error(err)
+    );
+  });
 };
 
 window.searchYouTube = searchYouTube;
